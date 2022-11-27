cfc20ba06ca3b51ea4dc2070bf1be94f32d39642
ajit0043 <ajittechanical@gmail.com>
2022-11-27 10:41:10 +0530

    check

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app= FastAPI()
 @app.get('/')
 def index():
     return { "hello" : {"ajit": "abhay"}}
+
+
+@app.get('/about')
+def about():
+    return{"ansh":"ansh ka birthday aane wala h 14th april ko"}
